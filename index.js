9655f629d1224fd3b2059ef0be49be0bfca013b3
taynguyen528 <phamtrinhtaynguyen2002@gmail.com>
2023-12-09 20:56:55 +0700

    push code

--- index.js
+++ index.js
@@ -12,36 +12,40 @@ function tinhDiem(a, b, c) {
 // Khu vực A: 2đ, B: 1đ, C: 0.5đ
 // Đối tượng: 1: 2.5đ, 2: 1,5đ, 3: 1đ.
 document.getElementById("btnTinhDiem").onclick = function () {
-  var khuVuc = Number(getID("options").value);
-  var doiTuong = Number(getID("studentType").value);
+  var khuVuc = getID("options").value;
+  var doiTuong = getID("studentType").value;
 
-  if (khuVuc !== " " && doiTuong !== "") {
-    var diemTT = Number(getID("diemHoiDong").value);
-    var diemThuNhat = Number(getID("monThuNhat").value);
-    var diemThuHai = Number(getID("monThuHai").value);
-    var diemThuBa = Number(getID("monThuBa").value);
+  if (khuVuc !== "" && doiTuong !== "") {
+    var diemTT = Number(document.getElementById("diemHoiDong").value);
+    var diemThuNhat = Number(document.getElementById("monThuNhat").value);
+    var diemThuHai = Number(document.getElementById("monThuHai").value);
+    var diemThuBa = Number(document.getElementById("monThuBa").value);
+
+    khuVuc = Number(khuVuc);
+    doiTuong = Number(doiTuong);
+
+    var check = diemThuNhat !== 0 && diemThuHai !== 0 && diemThuBa !== 0;
+    var diemTB = Number(
+      (
+        tinhDiem(diemThuNhat, diemThuHai, diemThuBa) +
+        doiTuong +
+        khuVuc
+      ).toFixed(2)
+    );
+
+    var text = getID("ketQua");
+    var textTT = getID("ketQuaTT");
+    var trungTuyen = " ";
+    if (diemTB >= diemTT && check) {
+      trungTuyen = "Trúng tuyển";
+    } else {
+      trungTuyen = "Không trúng tuyển";
+    }
+    text.innerHTML = diemTB + " điểm.";
+    textTT.innerHTML = trungTuyen;
   } else {
     alert("Vui lòng chọn cả khu vực và đối tượng của sinh viên!");
   }
-
-  var check = diemThuNhat !== 0 && diemThuHai !== 0 && diemThuBa !== 0;
-  // console.log(check);
-  var diemTB = Number(
-    (tinhDiem(diemThuNhat, diemThuHai, diemThuBa) + doiTuong + khuVuc).toFixed(
-      2
-    )
-  );
-
-  var text = getID("ketQua");
-  var textTT = getID("ketQuaTT");
-  var trungTuyen = " ";
-  if (diemTB >= diemTT && check) {
-    trungTuyen = "Trúng tuyển";
-  } else {
-    trungTuyen = "Không trúng tuyển";
-  }
-  text.innerHTML = diemTB + " điểm.";
-  textTT.innerHTML = trungTuyen;
 };
 
 // Bài 2: Tính tiền điện
@@ -112,4 +116,60 @@ document.getElementById("tinhThue").onclick = function () {
 
   var textThue = getID("thuePhaiTra");
   textThue.innerHTML = `Thuế thu nhập phải trả của ${hoTen}: ${thue.toLocaleString()} đồng.`;
+};
+
+// Bài 4: Tính tiền cáp
+function check() {
+  var loaiKH = document.getElementById("customerType").value;
+  var formketnoi = document.getElementById("form-ketnoi");
+
+  if (loaiKH === "0") {
+    formketnoi.classList.add("hidden");
+  } else {
+    formketnoi.classList.remove("hidden");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  check();
+  document.getElementById("customerType").addEventListener("change", check);
+});
+
+document.getElementById("tinhHoaDon").onclick = function () {
+  var maKH = getID("maKH").value;
+  var soKenh = getID("soKenhCaoCap").value * 1;
+  var loaiKH = getID("customerType").value * 1;
+  var loaiKhachHang = " ";
+  if (loaiKH == "1") {
+    loaiKhachHang = "Doanh Nghiệp";
+  } else {
+    loaiKhachHang = "Nhà Dân";
+  }
+
+  var tienCap = 0;
+
+  if (loaiKH == "1") {
+    var soKetNoi = getID("soKetNoi").value * 1;
+    var phiHoaDon = 15;
+    var phiCoBan = 0;
+
+    if (soKetNoi > 10) {
+      phiCoBan = 75 + (soKetNoi - 10) * 5;
+    } else {
+      phiCoBan = 0;
+    }
+    var phiCaoCap = 50 * soKenh;
+
+    tienCap = phiHoaDon + phiCoBan + phiCaoCap;
+  } else {
+    var phiHoaDon = 4.5;
+    var phiCoBan = 20.5;
+    var phiCaoCap = 7.5 * soKenh;
+
+    tienCap = phiHoaDon + phiCoBan + phiCaoCap;
+  }
+  var textHoaDon = getID("textHoaDon");
+  textHoaDon.innerHTML = `Mã khách hàng: ${maKH}<br>Loại khách hàng: ${loaiKhachHang}<br>Tổng tiền hóa đơn: $${tienCap.toFixed(
+    2
+  )}`;
 };
